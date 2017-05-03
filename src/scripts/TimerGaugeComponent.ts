0d78cd7c557896cc2bec6baee37c0c17dad51891
wakamsha <wakamsha@gmail.com>
2017-05-03 23:43:31 +0900

    feat(TimerGaugeComponent.ts): add timer gauge component

---
+++ src/scripts/TimerGaugeComponent.ts
@@ -0,0 +1,123 @@
+import {Observable} from 'rxjs';
+import {VNode, div, span} from '@cycle/dom';
+
+type Sources = {
+    props: {
+        maxTime$: Observable<number>;
+        alertTime: number;
+        active$: Observable<boolean>;
+        reset$: Observable<undefined>;
+    }
+}
+
+type Sinks = {
+    isActive$: Observable<boolean>;
+    timeSpent$: Observable<number>;
+    DOM: Observable<VNode>;
+}
+
+class State {
+    public isPlaying = false;
+    public timeSpent = 0;
+
+    private lastTime = -1;
+
+    public tick(): State {
+        const now = Date.now();
+        if (this.lastTime > 0) {
+            this.timeSpent += (now - this.lastTime);
+        }
+        this.lastTime = now;
+        return this;
+    }
+
+    public setPlaying(playing: boolean): State {
+        this.isPlaying = playing;
+        if (!this.isPlaying) {
+            this.lastTime = -1;
+        }
+        return this;
+    }
+
+    public reset(): State {
+        this.isPlaying = false;
+        this.timeSpent = 0;
+        this.lastTime = -1;
+        return this;
+    }
+}
+
+function digit(n: number): string {
+    return (`0${n}`).substr(-2);
+}
+
+function render({maxTime, time, alertTime}: {alertTime: number, maxTime: number, time: number}): VNode {
+    const min = digit(Math.floor(time / 60000));
+    const sec = digit(Math.floor(time / 1000) % 60);
+    const msec = digit(Math.floor(Math.floor(time / 10)));
+    const ratio = time / maxTime;
+    const isAlert = time < alertTime;
+
+    return div('.timer-gauge', [
+        div('.timer-gauge__col.timer-gauge__col--progress', [
+            div('.progress', [
+                div('.progress-bar', {
+                    style: {width: `${ratio * 100}%`},
+                    class: {
+                        'progress-bar-danger': isAlert,
+                    }
+                })
+            ])
+        ]),
+        div('.timer-gauge__col', [
+            span('.timer-gauge__counter', {
+                class: {
+                    'timer-gauge__counter--danger': isAlert
+                }
+            }, `${min}:${sec}:${msec}`)
+        ])
+    ]);
+}
+
+export function TimerGaugeComponent({props}: Sources): Sinks {
+    const active$ = props.active$;
+
+    const model$ = Observable.merge(
+        // tick$
+        Observable
+            .merge(
+                active$,
+                props.reset$ ? props.reset$.mapTo(false) : Observable.never()
+            )
+            .switchMap((active: boolean) => active ? Observable.interval(33) : Observable.of(0))
+            .map((_) => (acc: State) => acc.tick()),
+        // setPlaying$
+        active$.map((playing: boolean) => (acc: State) => acc.setPlaying(playing)),
+        // reset$
+        props.reset$ ? props.reset$.map((_) => (acc: State) => acc.reset()) : Observable.never()
+    ).startWith(
+        (seed: State) => seed
+    ).scan(
+        (acc: State, fn: (acc: State) => State) => fn(acc), new State()
+    );
+
+    const vdom$ = Observable.combineLatest(
+        model$,
+        props.maxTime$,
+        (state, maxTime) => {
+            const remainTime = maxTime - state.timeSpent;
+            return {
+                time: remainTime < 0 ? 0 : remainTime,
+                maxTime
+            };
+        }
+    ).distinctUntilChanged().map(
+        ({time, maxTime}): VNode => render({maxTime, time, alertTime: props.alertTime})
+    );
+
+    return {
+        isActive$: model$.map((state: State): boolean => state.isPlaying),
+        timeSpent$: model$.map((state: State): number => state.timeSpent / 1000),
+        DOM: <Observable<VNode>>vdom$
+    }
+}
--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,8 +1,9 @@
 import {Observable} from 'rxjs';
-import {VNode, div, makeDOMDriver, button, span, h4} from '@cycle/dom';
+import {VNode, div, makeDOMDriver, button, span, h4, h2, hr, pre, code, p} from '@cycle/dom';
 import {DOMSource} from '@cycle/dom/rxjs-typings';
 import {run} from '@cycle/rxjs-run';
 import {BackdropComponent} from './BackdropComponent';
+import {TimerGaugeComponent} from './TimerGaugeComponent';
 
 type Sources = {
     DOM: DOMSource;
@@ -14,7 +15,6 @@ type Sinks = {
 
 function main(sources: Sources): Sinks {
     const backdrop = BackdropComponent({
-        // DOM: sources.DOM,
         props: {
             transclude$: Observable.of(
                 div('.container', [
@@ -26,6 +26,9 @@ function main(sources: Sources): Sinks {
                                         span('×')
                                     ]),
                                     h4('.modal-title', 'Modal title')
+                                ]),
+                                div('.modal-body', [
+                                    p('Vivamus suscipit tortor eget felis porttitor volutpat. Donec sollicitudin molestie malesuada. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.')
                                 ])
                             ])
                         ])
@@ -39,14 +42,52 @@ function main(sources: Sources): Sinks {
         }
     });
 
+    const timerGauge = TimerGaugeComponent({
+        props: {
+            maxTime$: Observable.of(10000),
+            alertTime: 3000,
+            active$: Observable.merge(
+                sources.DOM.select('#timer-gauge-play').events('click').mapTo(true),
+                sources.DOM.select('#timer-gauge-pause').events('click').mapTo(false)
+            ),
+            reset$: sources.DOM.select('#timer-gauge-reset').events('click').mapTo(undefined)
+        }
+    });
+
     return {
         DOM: Observable.combineLatest(
             backdrop.DOM,
-            (backdropDOM) => {
-                return div([
-                    div('.container', [
+            timerGauge.DOM,
+            timerGauge.timeSpent$,
+            timerGauge.isActive$,
+            (backdropDOM, timerGaugeDOM, timeSpent, isActive) => {
+                return div('.container', [
+                    h2('.page-header', 'Backdrop'),
+                    div([
                         button('#dialog-open.btn.btn-default', 'Open'),
                         backdropDOM
+                    ]),
+                    h2('.page-header', 'Timer Gauge'),
+                    div('.row', [
+                        div('.col-sm-6', [
+                            timerGaugeDOM,
+                            hr(),
+                            div('.btn-group', [
+                                button('#timer-gauge-play.btn.btn-default', 'Play'),
+                                button('#timer-gauge-pause.btn.btn-default', 'Pause'),
+                                button('#timer-gauge-reset.btn.btn-default', 'Reset')
+                            ])
+                        ]),
+                        div('.col-sm-6', [
+                            pre([
+                                code([
+                                    JSON.stringify({
+                                        isActive: isActive,
+                                        result: timeSpent
+                                    }, null, 2)
+                                ])
+                            ])
+                        ])
                     ])
                 ])
             }
